0181a45366669bd366e540cdb35d34d81aaf8815
ramaTImati <rifqiramadhan1512@gmail.com>
2024-10-28 14:59:54 +0700

    update webhook and readme

--- src/config/waliby.php
+++ src/config/waliby.php
@@ -10,6 +10,8 @@ return [
     'phoneBookTable' => env('WALIBY_PHONE_BOOK', 'waliby'),
     'phoneNumberColumn' => env('WALIBY_PHONE_NUMBER_COLUMN', 'nomor'),
     'nameColumn' => env('WALIBY_NAME_COLUMN', 'nama'),
+    'webhookMessageId' => env('WALIBY_WEBHOOK_MESSAGE_ID_KEY', 'id'),
+    'webhookStatus' => env('WALIBY_WEBHOOK_STATUS_KEY', 'status'),
 
     /*
     |--------------------------------------------------------------------------
